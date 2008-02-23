23fd02f069e0fce5a622bcc0e2ee5c834ced6288
Dimitri Krassovski <labria@startika.com>
2008-02-24 00:29:43 +0300

    add logout button and map root to test_controller

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.root :controller => "welcome"
 
   # See how all your routes lay out with "rake routes"
-
+  map.connect '', :controller => "test"
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
