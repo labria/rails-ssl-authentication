0ee1c9179425559f0bc84d0354fe654c4af96627
Dimitri Krassovski <labria@startika.com>
2008-02-23 22:39:39 +0300

    test ssl in test_controller

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,2 +1,9 @@
 class TestController < ApplicationController
+  def index
+    if request.ssl?
+      render :text => "ssl request"
+    else
+      render :text => "non-ssl request"
+    end
+  end
 end
