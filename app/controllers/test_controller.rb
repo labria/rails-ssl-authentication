8f61865be4e9e7a20fdc3cabd749e2588841f54e
Dimitri Krassovski <labria@startika.com>
2008-02-25 19:08:21 +0300

    change welcome message

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,12 +1,4 @@
 class TestController < ApplicationController
   def index
-    if request.env["HTTP_X_SSL_CLIENT_S_DN"]
-      pairs = request.env["HTTP_X_SSL_CLIENT_S_DN"].split("/").
-        delete_if{ |x| x == ""}.
-        map{|p| p.split("=")}.flatten
-      @info = Hash.[](*pairs)
-    else
-      render :text => "No header found!"
-    end
   end
 end
